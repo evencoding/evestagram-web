3bdbdb751914a439d7ba4ae0c3dc9063e1a84a19
evencoding <88191233+evencoding@users.noreply.github.com>
2022-02-04 05:42:00 +0900

    Frontend Setup

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1``;
+
+const Container = styled.div``;
+
+function Home() {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/screens/Login.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1``;
+
+const Container = styled.div``;
+
+function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+    </Container>
+  );
+}
+
+export default Login;
